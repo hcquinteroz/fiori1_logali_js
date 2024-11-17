3f1a99d493dff2af953e188c5f9ab9d1b66eb622
hcquinteroz <hcquinteroz@gmail.com>
2024-11-17 01:21:48 +0000

    Binding y formateadores

--- SAPIU5/webapp/controller/InvoicesList.controller.js
+++ SAPIU5/webapp/controller/InvoicesList.controller.js
@@ -1,13 +1,17 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/ui/model/json/JSONModel',
-    '../model/InvoicesFormatter'
+    '../model/InvoicesFormatter',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+     * @param {typeof sap.ui.model.Filter} Filter
+     * @param {typeof sap.ui.model.FilterOperator} FilterOperator
      */
-    function (Controller, JSONModel, InvoicesFormatter) {
+    function (Controller, JSONModel, InvoicesFormatter, Filter, FilterOperator) {
         'use strict';
 
         return Controller.extend("logaligroup.SAPUI5.controller.InvoicesList", {
@@ -20,6 +24,24 @@ sap.ui.define([
                     eur: "EUR"
                 });
                 this.getView().setModel(oViewModel, "currency");
+            },
+
+            onFilterInvoices: function(oEvent) {
+                const aFilter = [];
+                const sQuery = oEvent.getParameter('query');
+
+                if (sQuery) {
+                    // aFilter.push(new Filter("ProductName", "EQ", sQuery));
+                    aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+                }
+
+                // Aplicamos el filtro al listado de productos
+                // Obtenemos desde la vista, el objeto con la lista
+                const oList = this.getView().byId("invoiceList");
+
+                // Obtenemos los ítems de la lista
+                const oBinding = oList.getBinding("items");
+                oBinding.filter(aFilter);
             }
         });
 
