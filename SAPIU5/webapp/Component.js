06fd4d7c6f8249c6503253b55e0e9676e647ea0a
Hern� Quintero <hcquinteroz@gmail.com>
2025-01-14 19:38:35 +0000

    Densidad de contenido

--- SAPIU5/webapp/Component.js
+++ SAPIU5/webapp/Component.js
@@ -2,13 +2,15 @@ sap.ui.define([
     'sap/ui/core/UIComponent',
     "logaligroup/SAPUI5/model/Models",
     "sap/ui/model/resource/ResourceModel",
-    "./controller/HelloDialog"
+    "./controller/HelloDialog",
+    "sap/ui/Device"
 ],
     /**
      * @param {typeof sap.ui.core.UIComponent} UIComponent
      * @param {typeof sap/ui/model/resource/ResourceModel} ResourceModel
+     * @param {typeof sap/ui/Device} Device
      */
-    function (UIComponent, Models, ResourceModel, HelloDialog) {
+    function (UIComponent, Models, ResourceModel, HelloDialog, Device) {
         'use strict';
 
         return UIComponent.extend("logaligroup.SAPUI5.Component", {
@@ -37,6 +39,17 @@ sap.ui.define([
             // Función de apertura del diálogo para ser invocada desde cualquier parte de la aplicación
             openHelloDialog: function () {
                 this._helloDialog.open();
+            },
+
+            // Función que retorna la clase CSS a utilizar para el manejo de la densidad de la información, 
+            // de acuerdo con el dispositivo
+            getContentDensityClass: function () {
+                if (!Device.support.touch) {
+                    this._sContentDensityClass = "sapUiSizeCompact";
+                } else {
+                    this._sContentDensityClass = "sapUiSizeCozy";
+                }
+                return this._sContentDensityClass;
             },
 
             // Liberamos los recursos que hemos creado
